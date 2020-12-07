850922530c7fcdd6dc1c32319049b1a3baa138dc
Hugo Seixas <tameirao.hugo@gmail.com>
2020-12-07 19:25:40 -0300

    Write functions to download .csv files.

--- 01_download_process.R
+++ 01_download_process.R
@@ -1,22 +1,58 @@
 
 # LOAD LIBRARIES --------------------------------------------------------------
 library(googledrive)
+library(fs)
+library(glue)
 library(tidyverse)
 
 # LOAD FILES FOM GOOGLEDRIVE --------------------------------------------------
 
 ## Authenticate googledrive ----
-googledrive::drive_auth(email = "tameirao.hugo@gmail.com")
+drive_auth(email = "tameirao.hugo@gmail.com")
 
 ## Search file ----
-file_id <- 
-  drive_ls("amazonia_project", pattern = ".csv") %>%
-  pull(id)
+file_list <- 
+  drive_ls(
+    "amazonia_project", 
+    pattern = ".csv", 
+    recursive = TRUE
+  )
 
-## Download file ----
-drive_download(file = as_id(file_id))
+## Create directory ----
+dir_create("data/")
+
+## Download files with purrr ----
+walk2(
+  .x = file_list$id,
+  .y = file_list$name,
+  .f = ~ { 
+    
+    drive_download(
+      file = as_id(.x), 
+      path = glue("data/{.y}"),
+      overwrite = TRUE
+    )
+    
+  }
+)
+
+## Download files with for loop ----
+for (f in seq_along(file_list$id)) {
+  
+  drive_download(
+    file = as_id(file_list$id[f]),
+    path = glue("data/{file_list$name[f]}"),
+    overwrite = TRUE
+  )
+  
+}
 
 # OPEN AND ORGANIZE FILE ----
 
-## Read file ----
-fires_df <- read_csv("fire_nrt_J1V-C2_168577.csv")
+## Read MODIS archive ----
+mod_arch <- read_csv("data/fire_archive_M6_168580.csv")
+
+## Read VIIRS archive ----
+virs_arch <- read_csv("data/fire_archive_V1_168582.csv")
+
+
